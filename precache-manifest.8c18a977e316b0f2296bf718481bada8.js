7adffb40555ed6210f9da703500adfcb8693d48a
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-05-08 12:51:13 -0600

    Updates

---
+++ precache-manifest.8c18a977e316b0f2296bf718481bada8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a93653b169fc9b7107b65b17798e8f",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "932786a681a3f0b7d660",
+    "url": "/reactPortfolio/static/css/main.d3bf9d18.chunk.css"
+  },
+  {
+    "revision": "761b4a5ee10766fee70e",
+    "url": "/reactPortfolio/static/js/2.e2ea6681.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPortfolio/static/js/2.e2ea6681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932786a681a3f0b7d660",
+    "url": "/reactPortfolio/static/js/main.ed169038.chunk.js"
+  },
+  {
+    "revision": "c633ece477021281c682",
+    "url": "/reactPortfolio/static/js/runtime-main.812acd26.js"
+  },
+  {
+    "revision": "0cb8b4ad0fd75e53e6a04e743e947dd0",
+    "url": "/reactPortfolio/static/media/bio.0cb8b4ad.png"
+  },
+  {
+    "revision": "04abacbf3b2208700eb6a359ff4ac386",
+    "url": "/reactPortfolio/static/media/forest.04abacbf.png"
+  },
+  {
+    "revision": "2b37954f37fa441f7aafab34f7050e3f",
+    "url": "/reactPortfolio/static/media/logo.2b37954f.svg"
+  },
+  {
+    "revision": "1ddf11fe9c52cf0d16e001a13f9da7fa",
+    "url": "/reactPortfolio/static/media/port1.1ddf11fe.png"
+  },
+  {
+    "revision": "b2844da4310a1d1175c3eb6cb4b08159",
+    "url": "/reactPortfolio/static/media/port2.b2844da4.png"
+  },
+  {
+    "revision": "c78ec2e99b63cd574faee31370755c72",
+    "url": "/reactPortfolio/static/media/port3.c78ec2e9.png"
+  },
+  {
+    "revision": "78b7bc238771f4eefba448d201fbb3e9",
+    "url": "/reactPortfolio/static/media/port4.78b7bc23.png"
+  },
+  {
+    "revision": "9a5639f31319904d2f8aecff633a75e4",
+    "url": "/reactPortfolio/static/media/port5.9a5639f3.png"
+  },
+  {
+    "revision": "73b04ca1bf1941a73187cfbd845a86cb",
+    "url": "/reactPortfolio/static/media/port6.73b04ca1.png"
+  },
+  {
+    "revision": "a1a1f30533ef1b62328b4880c3425ecb",
+    "url": "/reactPortfolio/static/media/smgresume.a1a1f305.pdf"
+  }
+]);
